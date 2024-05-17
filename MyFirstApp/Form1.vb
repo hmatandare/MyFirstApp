b3ee8fdf9984467b8530f29b6e2d851e75e3a326
harold <hmatandare@gmail.com>
2024-05-17 11:52:11 +0200

    First Git Project

---
+++ MyFirstApp/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        If btnTest.Enabled = True Then
+            MsgBox("Hello World")
+        Else
+
+
+        End If
+    End Sub
+End Class
